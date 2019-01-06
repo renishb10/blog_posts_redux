0a603f8136e9a5bdc70c9289d9138e9595eb2b0b
Renish B <renish.bhaskaran@globalenglish.com>
2019-01-06 13:17:13 +0530

    basic react implementations

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 //Import Components here
 import Post from './components/Post';
+import PostForm from './components/PostForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>Blog Post Redux Example</h2>
         </header>
+        <PostForm />
+        <hr />
         <Post />
       </div>
     );
